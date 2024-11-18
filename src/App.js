9fbbcb6d75efb480e004e6372f037247cc1f1087
pushpanjali29 <pushpanjali.0029@gamil.com>
2024-11-18 15:19:18 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,42 +1,47 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
   const [countries, setCountries] = useState([]);
+  const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    const fetchCountries = async () => {
-      try {
-        const response = await fetch('https://xcountries-backend.azurewebsites.net/all');
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        const data = await response.json();
+    fetch("https://xcountries-backend.azurewebsites.net/all")
+      .then((response) => response.json())
+      .then((data) => {
         setCountries(data);
-      } catch (error) {
-        console.error('Error fetching data: ', error);
-        setError(error.message);
-      }
-    };
-
-    fetchCountries();
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setError(error);
+        setLoading(false);
+      });
   }, []);
 
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
+
+  if (error) {
+    return <h2>Error fetching data: {error.message}</h2>;
+  }
+
   return (
-    <div className="App">
-      <h1>Country Flags</h1>
-      {error && <p>Error fetching countries: {error}</p>}
-      <div className="country-list">
-        {countries.map((country) => (
-          <div key={country.alpha3Code} className="country">
-            <img src={country.flag} alt={`Flag of ${country.name}`} className="flag" />
-            <p>{country.name}</p>
-          </div>
-        ))}
-      </div>
+    <div className="grid-container">
+      {countries.map((country) => (
+        <div className="country-box" key={country.name}>
+          <img
+            src={country.flag}
+            alt={`Flag of ${country.name}`}
+            className="flag"
+          />
+          <h3>{country.name}</h3>
+        </div>
+      ))}
     </div>
   );
-}
+};
 
-export default App;
+export default App
